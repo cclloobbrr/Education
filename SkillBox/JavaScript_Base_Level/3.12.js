ebfa009fad096a351cfd7f0719d95dfced9fa681
clobr <vvolodia479@gmail.com>
2025-05-20 18:54:24 +0300

    Add task

---
+++ SkillBox/JavaScript_Base_Level/3.12.js
@@ -0,0 +1,14 @@
+// Тернарный оператор
+// Замените конструкция if на тернарный оператор
+
+let randomNumber = 10
+console.log(`Произвольное число: ${randomNumber}`)
+
+// if (randomNumber > 20) {
+//   console.log("Число > 20")
+// } else {
+//   console.log("Число <= 20")
+// }
+
+let x = randomNumber > 20 ? `Число > 20` : `Число <= 20`;
+console.log(x);
