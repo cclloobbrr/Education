33ebcab1899408b12b53f5df393afffdf8b62697
clobr <vvolodia479@gmail.com>
2025-06-03 16:14:43 +0300

    add

---
+++ SkillBox/Javascript_Advanced_level/1.2/1.2.js
@@ -0,0 +1,8 @@
+//Обработчик событий на кнопке
+document.querySelector('button').addEventListener('clic', () => {});
+
+//
+window.addEventListener('resize', () => {});
+
+//
+document.addEventListener('DOMContentLoaded', () => {});
